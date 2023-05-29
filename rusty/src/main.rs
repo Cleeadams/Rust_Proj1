006c5978246e92ab0ada6a17ce5cc6d87b846015
Connor Adams <cleeadacon909@gmail.com>
2023-05-29 10:36:15 -0700

    Learn about variables and constants

--- rusty/src/main.rs
+++ rusty/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 4;
+    println!("x is: {}", x);
+
+    {
+        let y = 2;
+        println!("y is: {}", y);
+    }
+
+    x += 1;
+    println!("x is: {}", x);
+    main2()
+}
+
+fn main2() {
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
